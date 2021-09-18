2801adaf319ecc55efd8bf392c2a95892b8389c0
masai <varshakumari8999@gmail.com>
2021-09-19 00:20:25 +0530

    Api Call

--- Pizza/app/src/main/java/com/varsha/pizza/repository/PizzaRepository.kt
+++ Pizza/app/src/main/java/com/varsha/pizza/repository/PizzaRepository.kt
@@ -1,4 +1,23 @@
 package com.varsha.pizza.repository
 
-class PizzaRepository {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+import com.varsha.pizza.api.PizzaApiService
+import com.varsha.pizza.models.PizzaResponse
+
+class PizzaRepository(private val pizzaApiService: PizzaApiService) {
+    private val pizzaLiveData=MutableLiveData<PizzaResponse>()
+
+    val pizzas:LiveData<PizzaResponse>
+    get() = pizzaLiveData
+
+
+    suspend fun getPizza(){
+        val result= pizzaApiService.getPizza()
+        if (result?.body()!=null){
+            pizzaLiveData.postValue(result.body())
+
+        }
+    }
 }
